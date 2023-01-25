a64d49e5f400a07914ec6d3c922b45f5fde7c0ce
tristanmcooper <78113615+tristanmcooper@users.noreply.github.com>
2023-01-25 15:40:46 -0800

    checkpoint 1

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,18 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+
+    // Creating a temporary array
+    int[] tempArr = new int[arr.length];
+
+    // Copying over elements in reverse
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      tempArr[i] = arr[arr.length - i - 1];
+    }
+
+    // Deep copy of temporary array to original
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = tempArr[i];
     }
   }
 
@@ -14,9 +24,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,32 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  // My test case
+  // BUG: Cannot reverse the same list this way; a temp list is necessary
+  // because you are changing the values of the list as you are copying them,
+  // so by the time you reach one after the middle index, you are copying over
+  // elements that have already been copied, not the originals -- that data is 
+  // lost
+  @Test
+  public void testReverseInPlace2() {
+    int[] input2 = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input2);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  // My test case
+  // BUG: assigns values of the new array to the old one instead of vice-versa
+  // This way, the returned array will always be empty?
+  @Test
+  public void testReversed2() {
+    int[] input2 = {1,2,3,4,5};
+    assertArrayEquals(new int[] {5,4,3,2,1}, ArrayExamples.reversed(input2));
   }
 }
