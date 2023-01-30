6a7aca7eabc73f051afb05403188e156eab53a73
tristanmcooper <78113615+tristanmcooper@users.noreply.github.com>
2023-01-30 14:47:43 -0800

    geisel

--- report2/StringServer.java
+++ report2/StringServer.java
@@ -3,14 +3,19 @@ import java.net.URI;
 
 import java.util.ArrayList;
 
+// URLHandler in "Server.java"
 class Handler implements URLHandler {
 
-    ArrayList<String> list = new ArrayList<String>();
+    // Stores "single string"
+    // Chose to use an ArrayList to store the strings being added
+    ArrayList<String> singleString = new ArrayList<String>();
 
+    // Allows me to return a string in the next method
     public String printArray() {
         String finalString = "";
-        for (int i = 0; i < list.size(); i++) {
-            finalString += list.get(i)+"\n";
+        for (int i = 0; i < singleString.size(); i++) {
+            // Adds line break
+            finalString += singleString.get(i)+"\n";
         }
         return finalString;
     }
@@ -18,6 +23,7 @@ class Handler implements URLHandler {
     public String handleRequest(URI url) {
 
 
+        // No query, simply display the ArrayList (previous inputs)
         if (url.getPath().equals("/")) {
             String emptyRequest = printArray();
             return emptyRequest;
@@ -26,6 +32,7 @@ class Handler implements URLHandler {
         else {
             System.out.println("Path: " + url.getPath());
 
+            // Correct request
             if (url.getPath().contains("/add-message")) {
 
                 String[] parameters = url.getQuery().split("=");
@@ -33,13 +40,15 @@ class Handler implements URLHandler {
                 if (parameters[0].equals("s")) {
 
                     for (int i = 1; i < parameters.length; i++) {
-                        list.add(parameters[i]);
+                        // Everything after the "="
+                        singleString.add(parameters[i]);
                     }
 
                     String printedArray = printArray();
                     return printedArray;
                 }
             }
+            // Invalid request
             return "404 Not Found";
         }
     }
@@ -48,7 +57,8 @@ class Handler implements URLHandler {
 class StringServer extends Handler {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            System.out.println("Missing port number! Try any number between "
+            + "1024 to 49151");
             return;
         }
 
